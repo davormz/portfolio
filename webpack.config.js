629fd4dfacec426eab17852a18975a0aff415dab
David Ramirez <davo.rmz@gmail.com>
2021-03-10 20:17:53 -0600

    basic webpack configuration added

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+    },
+    resolve: {
+        extensions:['.js']
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        // compress: true,
+        port: 9000,
+        // after(app){
+        //   console.log('======== after ==========')
+        //   console.log(app)
+        //   // do fancy stuff
+        // },
+        // before(app) {
+        //   console.log('======== before =========')
+        //   console.log(app)
+        // }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {loader: 'babel-loader'}
+            },
+            {   
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './src/public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].[contenthash].css'
+        })
+        // ,
+        // new CopyWebpackPlugin({
+        //     patterns: [
+        //         {
+        //             from: path.resolve(__dirname, 'assets', 'images'),
+        //             to: 'assets/images'
+        //         }
+        //         // ,
+        //         // {
+        //         //     from: path.resolve(__dirname, 'assets', 'icons'),
+        //         //     to: 'assets/icons'
+        //         // }
+        //     ]
+        // })
+        // ,
+        // new BundleAnalyzerPlugin()
+    ]
+}
